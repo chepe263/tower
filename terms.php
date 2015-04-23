3a68f551c714b49524e820fd5319b601c2af4eaa
Guillermo Chavez <chepe263@gmail.com>
2015-04-23 13:23:10 -0600

    Dollar sign is pegado

--- terms.php
+++ terms.php
@@ -51,7 +51,7 @@
 		<div class="large-12 panel">
 			<h2>100 Lugares para ver</h2>
 			<!--p><i>El mundo esta en tus manos</i></p-->
-			<h3>Gánate un viaje para 2 personas más $ 1,000 para gastos</h3>
+			<h3>Gánate un viaje para 2 personas más $1,000 para gastos</h3>
 			<br />
 			<br />
 			<h4>Mecanica de la promoción</h4>
