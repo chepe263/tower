2e5c475473a610f78a88cef97e9198570b12b276
Guillermo Chavez <gchavez@infonet.com.gt>
2015-04-23 09:56:07 -0600

    index

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+    
+    <div class="row">
+      <div class="large-12 columns">
+        <img src="img/contraportadas/01-Towerbank-100paises-generico copy.jpg" />
+      </div>
+    </div>
+    
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
